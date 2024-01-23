c3922d4f55f9b715834acf1478f981ba025d42a1
KaraChicken <ed29470381@gmail.com>
2024-01-23 16:31:02 +0800

    修改輪播圖、新增樣式、增加文案

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,8 @@ VApp
   RouterView
 </template>
 
+<style scoped>
+.v-main {
+  background: hsl(6, 16%, 64%);
+}
+</style>
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -2,43 +2,55 @@
 //- 輪播圖片素材取自部落客 圍事小哥
 //- 版權皆由原作者持有，此素材僅做學習用途非商業使用
 //- https://www.walkerland.com.tw/article/view/354452
-Swiper(pagination="pagination" :modules="modules" class="mySwiper")
-  SwiperSlide 
-    Img(src="../src/assets/1.jpg")
-  SwiperSlide 
-    Img(src="../src/assets/2.jpg")
-  SwiperSlide 
-    Img(src="../src/assets/3.jpg")
-  SwiperSlide 
-    Img(src="../src/assets/4.jpg")
-  SwiperSlide 
-    Img(src="../src/assets/5.jpg")
+Swiper(
+    :spaceBetween="30"
+    :centeredSlides="true"
+    :scrollbar={
+      hide: true,
+    }
+    :autoplay={
+      delay: 3500,
+      disableOnInteraction: false,
+    }
+    :navigation="true"
+    :modules="modules"
+    class="mySwiper"
+)
+  SwiperSlide(v-for="(slide, index) in [1, 2, 3, 4, 5]" :key="index")
+    Img(:src="`../src/assets/${slide}.jpg`")
+
+P 融合客棧的獨特元素，精心特調的燒烤醬料宛如一瓶難得的陳年美酒，經過大廚巧妙調製，
+  | 猶如客棧內的珍貴珍藏，每一滴都沉澱著風華絕代的瑰麗故事。
+P 烈火在燒烤爐中優雅舞動，仿佛客棧內的燈火閃爍，構築出一種溫馨而充滿情調的氛圍。
+P 食材在恰到好處的火侯下，彷彿在客棧那段美好時光中悠然綻放，每一口都是一曲對旅途美好回憶的味蕾奏鳴曲。
 </template>
 
 <script setup>
   import { Swiper, SwiperSlide } from 'swiper/vue';
   import 'swiper/css';
+  import 'swiper/css/pagination';
   import 'swiper/css/navigation';
-  import { Navigation } from 'swiper/modules';
-  
-  const  modules = [Navigation];
+  import 'swiper/css/scrollbar';
+  import { Autoplay, Scrollbar, Pagination, Navigation } from 'swiper/modules';
 
+  const modules = [Autoplay, Scrollbar, Pagination, Navigation];
 </script>
 
 <style scoped>
 .mySwiper {
-  width: 100vw;
-  height: 100vh;
+  height: calc(100vh - 64px);
+  width: 100%;
 }
 
 .mySwiper .swiper-slide {
-  width: 100vw;
-  height: 100vh;
+  height: 100%;
+  width: 100%;
 }
 
 .mySwiper .swiper-slide img {
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
+  background-size:cover;
   object-fit: cover; 
 }
 </style>

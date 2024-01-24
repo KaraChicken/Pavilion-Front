b89fdd008c0dc45aa0ef4377e71be444f5ec2abe
KaraChicken <ed29470381@gmail.com>
2024-01-24 13:55:32 +0800

    新增地圖

--- src/views/front/MapView.vue
+++ src/views/front/MapView.vue
@@ -1,3 +1,7 @@
 <template lang="pug">
-H1 行蹤地圖
+VCantainer
+  VRow(style="height: 100%;")
+    VCol(class="d-flex justify-center")
+      Div(style="width: 52rem; height: 52rem;")
+        Img(src="./src/assets/Map.jpg" style="height: 100%;")
 </template>

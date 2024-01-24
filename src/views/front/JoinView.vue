a3f626659d767bc406750175ae663201e390e8e5
KaraChicken <ed29470381@gmail.com>
2024-01-24 13:33:41 +0800

    增加一些文案

--- src/views/front/JoinView.vue
+++ src/views/front/JoinView.vue
@@ -1,3 +1,12 @@
 <template lang="pug">
-H1 同道相邀
+VCantainer
+  VRow(style="height: 100%;")
+    VCol(cols="12" class="d-flex justify-center align-center")
+      Div(class="pa-5" style="width: 1200px; height: 700px; border-radius: 20px; background-color: #a38a66;")
+        H1(class="text-center mb-10") 同道相邀
+        Div(class="d-flex justify-center align-center" style="background-color= #dac9b1;")
+          H1 酒俠之長 &nbsp;&nbsp; 80000
+          H1 酒俠之副 &nbsp;&nbsp; 60000
+          H1 匠心廚俠 &nbsp;&nbsp; 50000
+          H1 客俠之風 &nbsp;&nbsp; 45000
 </template>

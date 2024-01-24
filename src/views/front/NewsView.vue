60998b0b4b9e01f50ba729693ee43602464e5660
KaraChicken <ed29470381@gmail.com>
2024-01-24 11:50:44 +0800

    武林告示新增資料表格

--- src/views/front/NewsView.vue
+++ src/views/front/NewsView.vue
@@ -1,4 +1,28 @@
 <template lang="pug">
-h1 武林告示
+VCantainer
+  VRow(style="height: 100%;")
+    VCol(cols="12" class="d-flex justify-center align-center")
+      Div(class="pa-5" style="width: 1200px; height: 700px; border-radius: 20px; background-color: #a38a66;")
+        H1(class="text-center mb-10") 武林告示
+        //- 這邊可能要串接後端接收資料
+        Div(class="ml-5" style="background-color= #dac9b1;")
+          VDataTable(:items="items")
 </template>
 
+<script setup>
+  const items = [
+    {
+      '標題': 'African Elephant',
+      '內容': 'Loxodonta africana',
+      '告示人': 'Herbivore',
+      '更新日期': 'Savanna, Forests',
+    },
+    {
+      '標題': 'African Elephant',
+      '內容': 'Loxodonta africana',
+      '告示人': 'Herbivore',
+      '更新日期': 'Savanna, Forests',
+    },
+    // ... more items
+  ]
+</script>

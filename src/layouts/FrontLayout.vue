d0121c49d11cdc481ecdd2201790cd509204beba
KaraChicken <ed29470381@gmail.com>
2024-01-25 14:38:56 +0800

    顏色設定

--- src/layouts/FrontLayout.vue
+++ src/layouts/FrontLayout.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
-VAppBar(color="primary")
+VAppBar(color="secondary")
   VContainer.d-flex.align-center.justify-center
-    VBtn(to="/news")
-      VAppBarTitle(class="text01") 最新消息(武林告示)
-    VBtn(to="/map")
-      VAppBarTitle(style="color: #D9CD90;") 店家地圖(行蹤地圖)
-    VBtn(to="/about")
-      VAppBarTitle(style="color: #D9CD90;") 關於我們(江湖緣起)
-    VBtn(to="/")
-      VAppBarTitle(style="color: #D9CD90;") 炙醉亭
-    VBtn(to="/reservation")
-      VAppBarTitle(style="color: #D9CD90;") 線上訂位(俠客預約)
-    VBtn(to="/menu")
-      VAppBarTitle(style="color: #D9CD90;") 線上菜單(英雄食典)
-    VBtn(to="/join")
-      VAppBarTitle(style="color: #D9CD90;") 加入我們(同道相邀)
+    VBtn(to="/news" color="text01")
+      VAppBarTitle 最新消息(武林告示)
+    VBtn(to="/map" color="text01")
+      VAppBarTitle 店家地圖(行蹤地圖)
+    VBtn(to="/about" color="text01")
+      VAppBarTitle 關於我們(江湖緣起)
+    VBtn(to="/" color="text01")
+      VAppBarTitle 炙醉亭
+    VBtn(to="/reservation" color="text01")
+      VAppBarTitle 線上訂位(俠客預約)
+    VBtn(to="/menu" color="text01")
+      VAppBarTitle 線上菜單(英雄食典)
+    VBtn(to="/join" color="text01")
+      VAppBarTitle 加入我們(同道相邀)
       
 //- 內容
 VMain

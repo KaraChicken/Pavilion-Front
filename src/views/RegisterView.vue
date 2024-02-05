2bb039fdb4e85b063f4a1304034814a0ba22dc80
KaraChicken <ed29470381@gmail.com>
2024-02-05 16:33:50 +0800

    註冊畫面

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+VCantainer
+  VRow(style="height: 100%;")
+    VCol.d-flex.justify-center.align-center(cols="12")
+      Div(class="pa-5" style="width: 75rem; height: 43.75rem; border-radius: 20px; background-color: #a38a66;")
+        H1(class="text-center mb-10") 同道相邀
+        Div.d-flex.justify-center.align-center(style="background-color= #dac9b1;")
+          VTextField(label="帳號")
+          VTextField(label="密碼")
+</template>

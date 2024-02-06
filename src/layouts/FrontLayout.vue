cb32118c0c3fd898e313e575426a49d8eb786367
KaraChicken <ed29470381@gmail.com>
2024-02-06 16:31:34 +0800

    使用sweetAlert

--- src/layouts/FrontLayout.vue
+++ src/layouts/FrontLayout.vue
@@ -13,12 +13,10 @@ import { useDisplay } from 'vuetify'
 import { ref, computed } from 'vue'
 import { useUserStore } from '@/store/user'
 import { useApi } from '@/composables/axios'
-import { useSnackbar } from 'vuetify-use-dialog'
 import { useRouter } from 'vue-router'
 
 const { apiAuth } = useApi()
 const router = useRouter()
-const createSnackbar = useSnackbar()
 const user = useUserStore()
 
 // 導覽列
@@ -30,8 +28,8 @@ const navItems = computed(() => {
     { to: "/", text: "炙醉亭", show: !user.isLogin },
     { to: "/reservation", text: "線上訂位(俠客預約)", show: user.isLogin },
     { to: "/menu", text: "線上菜單(英雄食典)", show: !user.isLogin },
-    { to: "/login", text: "登入(同道相邀)", show: !user.isLogin }
-    // { to: "/logout", text: "登出", show: user.isLogin }
+    { to: "/login", text: "登入(同道相邀)", show: !user.isLogin },
+    { to: "/logout", text: "登出", show: user.isLogin }
   ]
 })
 
@@ -41,27 +39,27 @@ const logout = async () => {
     await apiAuth.delete('/user/logout')
     // 呼叫 store 的 logout() 函式
     user.logout()
-    createSnackbar({
-      text: '登出成功',
-      showCloseButton: false,
-      snackbarProps: {
-        timeout: 5000,
-        color: 'Success',
-        location: 'bottom'
-      }
-    })
+    // createSnackbar({
+    //   text: '登出成功',
+    //   showCloseButton: false,
+    //   snackbarProps: {
+    //     timeout: 5000,
+    //     color: 'Success',
+    //     location: 'bottom'
+    //   }
+    // })
     router.push('/')
   } catch (error) {
     const text = error?.response?.dara?.message || '發生錯誤，請稍後再試'
-    createSnackbar({
-      text,
-      showCloseButton: false,
-      snackbarProps: {
-        timeout: 2000,
-        color: 'red',
-        location: 'bottom'
-      }
-    })
+    // createSnackbar({
+    //   text,
+    //   showCloseButton: false,
+    //   snackbarProps: {
+    //     timeout: 2000,
+    //     color: 'red',
+    //     location: 'bottom'
+    //   }
+    // })
   }
 }
 

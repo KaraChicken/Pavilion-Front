16506a85febcf9f0dd3b0ae38c285d5b13a3e0d9
KaraChicken <ed29470381@gmail.com>
2024-02-06 16:32:11 +0800

    身分驗證

--- src/store/user.js
+++ src/store/user.js
@@ -1,8 +1,12 @@
+// Utilities
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import UserRole from '@/enums/UserRole'
+import { useApi } from '@/composables/axios'
 
 export const useUserStore = defineStore('user', () => {
+  const { apiAuth } = useApi()
+
   const token = ref('')
   const account = ref('')
   const email = ref('')
@@ -24,6 +28,43 @@ export const useUserStore = defineStore('user', () => {
   })
 
   const isAdmin = computed(() => {
-    return token.value.length > 0
+    return role.value === UserRole.ADMIN
   })
+
+  const getProfile = async () => {
+    if (token.value.length === 0) return
+
+    try {
+      const { data } = await apiAuth.get('/users/me')
+      login(data.result)
+    } catch (error) {
+      logout()
+    }
+  }
+
+  const logout = () => {
+    token.value = ''
+    account.value = ''
+    email.value = ''
+    cart.value = 0
+    role.value = UserRole.USER
+  }
+
+  return {
+    token,
+    account,
+    email,
+    cart,
+    role,
+    login,
+    logout,
+    isLogin,
+    isAdmin,
+    getProfile
+  }
+}, {
+  persist: {
+    key: '20240103',
+    paths: ['token']
+  }
 })

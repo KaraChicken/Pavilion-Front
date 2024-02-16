3bb682edfcacc1ad61a2bc42922d161eb13c8813
KaraChicken <ed29470381@gmail.com>
2024-02-16 16:18:09 +0800

    新增最新消息頁面

---
+++ src/views/admin/NewsView.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+VTable
+  Thead
+    Tr
+      Th(class="text-left") 姓名
+      Th(class="text-left") 訂位
+      Th(class="text-left") 備註
+  Tbody
+    Tr(
+      v-for="items in desserts"
+      :key="item.name"
+    )
+      Td {{ item.name }}
+      Td {{ item.calories }}
+</template>
+
+<script setup>
+  const desserts = () => {
+    [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+      },
+      {
+        name: 'Eclair',
+        calories: 262,
+      }
+    ]
+  }
+</script>

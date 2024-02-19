eb9202863c4eb1d245e3b47619ad62d16f25358c
KaraChicken <ed29470381@gmail.com>
2024-02-19 16:20:29 +0800

    複製樣板

--- src/views/front/ReservationView.vue
+++ src/views/front/ReservationView.vue
@@ -1,4 +1,4 @@
-<template lang="pug">
+<!-- <template lang="pug">
 VContainer(style="height:100%;" class="d-flex justify-center align-center")
   Div(class="pa-5 bg-tertiary custom-scrollbar" style="width: 75rem; height: 43.75rem; border-radius: 20px; text-align: center;")
     H1(class="text-center mb-10" style="color: #261E47;") 線上訂位
@@ -81,4 +81,58 @@ VContainer(style="height:100%;" class="d-flex justify-center align-center")
   // const resetValidation = {
   //   this: form.resetValidation()
   // }
+</script> -->
+<template lang="pug">
+VContainer
+  VRow
+    VCol(cols="12")
+      h1 訂單
+    VCol(cols="12")
+      VDataTable(:items="orders" :headers="headers")
+        template(#[`item.createdAt`]="{ item }")
+          | {{ new Date(item.createdAt).toLocaleString() }}
+        template(#[`item.cart`]="{ item }")
+          ul
+            li(v-for="cart in item.cart" :key="cart._id")
+              | {{ cart.product.name }} * {{ cart.quantity }}
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useApi } from '@/composables/axios'
+import Swal from 'sweetalert2'
+
+const { apiAuth } = useApi()
+
+const orders = ref([])
+const headers = [
+  { title: '訂單編號', key: '_id' },
+  { title: '日期', key: 'createdAt' },
+  { title: '商品', key: 'cart', sortable: false },
+  {
+    title: '金額',
+    key: 'price',
+    value: (item) => {
+      return item.cart.reduce((total, current) => {
+        return total + current.quantity * current.product.price
+      }, 0)
+    }
+  }
+]
+
+onMounted(async () => {
+  try {
+    const { data } = await apiAuth.get('/orders')
+    orders.value.push(...data.result)
+  } catch (error) {
+    const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: text,
+      showConfirmButton: false,
+      timer: 2000
+    });
+  }
+})
 </script>

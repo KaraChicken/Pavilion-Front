142dd2428ba46ebbdd6dc0e4e43fb29ab291c7bf
KaraChicken <ed29470381@gmail.com>
2024-02-19 16:15:21 +0800

    修改路由按鈕顯示條件

--- src/layouts/FrontLayout.vue
+++ src/layouts/FrontLayout.vue
@@ -25,12 +25,12 @@ const user = useUserStore()
 // 導覽列
 const navItems = computed(() => {
     return [
-    { to: "/news", text: "最新消息(武林告示)", show: !user.isLogin },
-    { to: "/map", text: "店家地圖(行蹤地圖)", show: !user.isLogin },
-    { to: "/about", text: "關於我們(江湖緣起)", show: !user.isLogin },
-    { to: "/", text: "炙醉亭", show: !user.isLogin },
-    { to: "/reservation", text: "線上訂位(俠客預約)", show: user.isLogin && !user.isAdmin },
-    { to: "/menu", text: "線上菜單(英雄食典)", show: !user.isLogin },
+    { to: "/news", text: "最新消息(武林告示)", show: (!user.isLogin || user.isLogin) || user.isAdmin },
+    { to: "/map", text: "店家地圖(行蹤地圖)", show: (!user.isLogin || user.isLogin) && !user.isAdmin },
+    { to: "/about", text: "關於我們(江湖緣起)", show: (!user.isLogin || user.isLogin) && !user.isAdmin },
+    { to: "/", text: "炙醉亭", show: (!user.isLogin || user.isLogin) && !user.isAdmin },
+    { to: "/reservation", text: "線上訂位(俠客預約)", show: user.isLogin || !user.isAdmin },
+    { to: "/menu", text: "線上菜單(英雄食典)", show: (!user.isLogin || user.isLogin) || user.isAdmin },
     { to: "/login", text: "登入(同道相邀)", show: !user.isLogin },
     { to: '/admin', text: '管理', show: user.isLogin && user.isAdmin }
   ]
@@ -38,16 +38,34 @@ const navItems = computed(() => {
 
 const logout = async () => {
   try {
-    // 這邊的 delete 為 axios 方法，apiAuth連結至axios檔案取得 .env.development 的值
-    await apiAuth.delete('/users/logout')
-    // 呼叫 store 的 logout() 函式
-    user.logout()
     Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "登出成功",
-      showConfirmButton: false,
-      timer: 1500
+      title: "登出嗎?",
+      text: "按下確定鍵登出。",
+      icon: "question",
+      showDenyButton: true,
+      confirmButtonText: "確定",
+      denyButtonText: `取消`
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "登出成功",
+          text: "您即將登出...",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 800
+        })
+        // 這邊的 delete 為 axios 方法，apiAuth連結至axios檔案取得 .env.development 的值
+        await apiAuth.delete('/users/logout')
+        // 呼叫 store 的 logout() 函式
+        user.logout()
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "登出取消",
+          icon: "info",
+          showConfirmButton: false,
+          timer: 500
+        })
+      }
     });
     router.push('/')
   } catch (error) {

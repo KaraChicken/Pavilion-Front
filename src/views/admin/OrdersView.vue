e80da0c0c645fba794e331b8fe6870fe4931e593
KaraChicken <ed29470381@gmail.com>
2024-02-20 16:37:58 +0800

    訂單管理修改

--- src/views/admin/OrdersView.vue
+++ src/views/admin/OrdersView.vue
@@ -2,15 +2,16 @@
 VContainer
   VRow
     VCol(cols="12")
-      h1 訂單
-    VCol(cols="12")
-      VDataTable(:items="orders" :headers="headers")
-        template(#[`item.createdAt`]="{ item }")
-          | {{ new Date(item.createdAt).toLocaleString() }}
-        template(#[`item.cart`]="{ item }")
-          ul
-            li(v-for="cart in item.cart" :key="cart._id")
-              | {{ cart.product.name }} * {{ cart.quantity }}
+      h1.text-center 訂單管理
+    VDivider
+      VCol(cols="12")
+        VDataTable(:items="orders" :headers="headers")
+          template(#[`item.createdAt`]="{ item }")
+            | {{ new Date(item.createdAt).toLocaleString() }}
+          template(#[`item.cart`]="{ item }")
+            ul
+              li(v-for="cart in item.cart" :key="cart._id")
+                | {{ cart.product.name }} * {{ cart.quantity }}
 </template>
 
 <script setup>
@@ -47,6 +48,7 @@ onMounted(async () => {
       icon: "error",
       title: "喔喔！！",
       text: text,
+      confirmButtonText: "確定"
     })
   }
 })

a492e566072f9322d601492bc4726c88c0ad6ef8
KaraChicken <ed29470381@gmail.com>
2024-02-20 16:38:16 +0800

    菜單畫面修改

--- src/views/front/MenuView.vue
+++ src/views/front/MenuView.vue
@@ -1,8 +1,47 @@
 <template lang="pug">
 VCantainer
-  H1(class="text-center") 英雄食典
-  VRow
-    VCol(v-for="(img, index) in 15" :key="index")
-      Div(style="width: 5rem; height: 5rem;")
-        Img(style="width: 100%; height: 100%" :src="`../src/assets/BBQ/${img}.jpg`")
+  VRow.justify-center.px-15
+    VCol.text-center.my-5(cols="12")
+      H1 英雄食典
+    VCol(cols="12" md="6" lg="3" v-for="product in products" :key="product._id")
+      ProductCard(v-bind="product")
 </template>
+
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { useApi } from '@/composables/axios.js'
+import Swal from 'sweetalert2'
+import ProductCard from '@/components/ProductCard.vue'
+import gsap from 'gsap'
+
+const { api } = useApi()
+
+const products = ref([])
+
+onMounted(async() => {
+  try {
+    const { data } = await api.get('/products', {
+      params: {
+        itemsPerPage: -1
+      }
+    })
+    products.value.push(...data.result.data)
+    await nextTick()
+    gsap
+      .to('.product-card', { opacity: 1, duration: 0.5 })
+  } catch (error) {
+    console.log(error)
+    const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+    Swal.fire({
+      icon: 'error',
+      title: text,
+      confirmButtonText: "確定"
+    })
+  }
+})
+</script>
+
+<style scoped lang="sass">
+.product-card
+  opacity: 0
+</style>

3f8f05c190c8d31126045c7d1f91ab647ba08a3a
KaraChicken <ed29470381@gmail.com>
2024-02-21 16:05:32 +0800

    增加sass樣式，微調畫面

--- src/views/front/AboutView.vue
+++ src/views/front/AboutView.vue
@@ -1,12 +1,23 @@
 <template lang="pug">
-H1(class="text-center my-5") 江湖緣起
-Div(class="text-center text-h4" )
-  P 江湖間有一處獨特的聚點，名為「炙醉亭」。
-  P 這是一座隱匿在武林深處的小亭，門前總是瀰漫著烤肉的誘人香氣，亭內的燈光昏黃，迎接著武者們歸來的腳步。
-  P 「炙醉亭」以烤肉為主，每一塊肉都是匠心獨運，經過火焰的熏烤，帶著淡淡的炭香，令人回味無窮。
-  P 在這裡武者們能夠盡情放鬆、品味美食，分享江湖奇聞，激發彼此之間的豪情壯志。
-  P 不僅如此，我們也提供了各式美酒，每一杯都是經過巧手調配，為武者們帶來一絲絲的舒適和滿足。
-  P 亭內武士們酒酣耳熱，交換著彼此的武藝心得，共同譜寫著一段段江湖傳奇。
-  P 「炙醉亭」不僅是一個品味美食的場所，更是武林人士情感交流的樞紐。
-  P 在這裡，烤肉的香氣、美酒的芬芳交織在一起，營造出一個充滿武俠風情的小天地。
+VCantainer
+  VRow(style="height: 100%;")
+    VCol(cols="12" class="d-flex justify-center align-center")
+      Div.pa-5.overflow( style="width: 75rem; height: 43.75rem; border-radius: 20px; background-color: #CAAD5F;" )
+        H1(class="text-center mb-10" style="color: #261E47;") 江湖緣起
+        Div(class="text-center text-h4")
+          P 江湖間有一處獨特的聚點，名為「炙醉亭」。
+          P 這是一座隱匿在武林深處的小亭，門前總是瀰漫著烤肉的誘人香氣，亭內的燈光昏黃，迎接著武者們歸來的腳步。
+          P 「炙醉亭」以烤肉為主，每一塊肉都是匠心獨運，經過火焰的熏烤，帶著淡淡的炭香，令人回味無窮。
+          P 在這裡武者們能夠盡情放鬆、品味美食，分享江湖奇聞，激發彼此之間的豪情壯志。
+          P 不僅如此，我們也提供了各式美酒，每一杯都是經過巧手調配，為武者們帶來一絲絲的舒適和滿足。
+          P 亭內武士們酒酣耳熱，交換著彼此的武藝心得，共同譜寫著一段段江湖傳奇。
+          P 「炙醉亭」不僅是一個品味美食的場所，更是武林人士情感交流的樞紐。
+          P 在這裡，烤肉的香氣、美酒的芬芳交織在一起，營造出一個充滿武俠風情的小天地。
 </template>
+
+<style scoped lang="sass">
+.overflow
+  overflow: scroll
+.overflow::-webkit-scrollbar 
+  display: none
+</style>

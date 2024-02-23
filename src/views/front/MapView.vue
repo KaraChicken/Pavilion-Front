788daea79b90cf92ceeef07e37e14b145132ef23
KaraChicken <ed29470381@gmail.com>
2024-02-23 16:44:43 +0800

    註解內連圖片

--- src/views/front/MapView.vue
+++ src/views/front/MapView.vue
@@ -3,5 +3,5 @@ VCantainer
   VRow(style="height: 100%;")
     VCol(class="d-flex justify-center")
       Div(style="width: 52rem; height: 52rem;")
-        Img(src="./src/assets/Map.jpg" style="height: 100%;")
+        //- Img(src="./src/assets/Map.jpg" style="height: 100%;")
 </template>

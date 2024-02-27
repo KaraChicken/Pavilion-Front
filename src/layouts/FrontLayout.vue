34ff451d452db9bf379d61ff6eef8d89440c1b0e
KaraChicken <ed29470381@gmail.com>
2024-02-27 16:03:43 +0800

    修改路徑

--- src/layouts/FrontLayout.vue
+++ src/layouts/FrontLayout.vue
@@ -29,13 +29,13 @@ VNavigationDrawer(v-model="drawer" temporary location="right" v-if="isMobile" co
       template(#append)
         VAppBarTitle 登出
 //- 導覽列
-VImg#logo(src="../../src/assets/BackGround/logoBanner.png")
-Div#eaves(style="background: url('../assets/BackGround/bg1.png')")
-Div.bambooLeft(id="leaf1" style="background: url('https://karachicken.github.io/Pavilion-Front/assets/BackGround/bg2.png') no-repeat center/cover")
-Div.bambooRight(style="background: url('https://karachicken.github.io/Pavilion-Front/assets/BackGround/bg2.png') no-repeat center/cover")
-Div.bambooCenterLeft(style="background: url('https://karachicken.github.io/Pavilion-Front/assets/BackGround/bg2.png') no-repeat center/cover")
-Div.bambooCenterLeft2(style="background: url('https://karachicken.github.io/Pavilion-Front/assets/BackGround/bg2.png') no-repeat center/cover")
-Div.bambooCenterRight(style="background: url('https://karachicken.github.io/Pavilion-Front/assets/BackGround/bg2.png') no-repeat center/cover")
+VImg#logo(src="@/assets/BackGround/logoBanner.png")
+Div#eaves(style="background: url('/src/assets/BackGround/bg1.png')")
+Div.bambooLeft(id="leaf1" style="background: url('/src/assets/BackGround/bg2.png') no-repeat center/cover")
+Div.bambooRight(style="background: url('/src/assets/BackGround/bg2.png') no-repeat center/cover")
+Div.bambooCenterLeft(style="background: url('/src/assets/BackGround/bg2.png') no-repeat center/cover")
+Div.bambooCenterLeft2(style="background: url('/src/assets/BackGround/bg2.png') no-repeat center/cover")
+Div.bambooCenterRight(style="background: url('/src/assets/BackGround/bg2.png') no-repeat center/cover")
 //- VImg(src="../../src/assets/Chinese_style_eaves.png" style="position:fixed;top:0;left:0;width:100%;z-index:10000")
 VAppBar(color="secondary" style="position:fixed;top:45px;")
   //- 手機版

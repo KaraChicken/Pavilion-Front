30d6d8c2b11e78c17e811465e313ff86cf00ce9e
KaraChicken <86883208+KaraChicken@users.noreply.github.com>
2024-04-10 15:59:46 +0000

    Deploying to gh-pages from @ KaraChicken/Pavilion-Front@c5c4746bdfe0c2fbf33cb729e4983f3e50346318 🚀

--- assets/scopeId-6f4bf6ba.js
+++
@@ -1,1 +0,0 @@
-import{az as o}from"./index-16fd70aa.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ assets/tag-7b201130.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index-87df4dfe.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
